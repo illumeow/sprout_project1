90a60c77de9104e056001de1642df7bba06b9762
illumeow <illumeow0223@gmail.com>
2023-04-24 01:19:39 +0800

    add ai concept

--- release/src/ai.cpp
+++ release/src/ai.cpp
@@ -2,6 +2,13 @@
 
 void ai(Pos& pos1, Pos& pos2) {
   // TODO: Task 4.
+  /*
+  初步想法: 
+  優先消除特殊寶石，掃過盤面知道每種寶石各有幾個
+  之後用"能消除多少寶石"來決定要消除哪個特殊寶石
+  若沒有特殊寶石則試著做出來，都做不出來就隨機選一個消除?
+  不確定能不能預測drop以後的結果(不含新生成的)來決定
+  */
   // You should remove the code below and determine the four values by yourself.
   pos1.x = gen_rand() % BOARD_HEIGHT;
   pos1.y = gen_rand() % BOARD_WIDTH;
